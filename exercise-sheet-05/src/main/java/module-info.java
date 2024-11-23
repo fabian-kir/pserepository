ecfc78df5e64bcd6c42e00ce959b7f9120251f76
Maukiii <mika.hepper@web.de>
2024-11-23 18:53:52 +0100

    requires hinzugefügt um ausführen zu können

--- exercise-sheet-05/src/main/java/module-info.java
+++ exercise-sheet-05/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 open module exercise {
 	requires de.hamstersimulator.objectsfirst.main;
+    requires javafx.controls;
 
-	exports de.unistuttgart.iste.sqa.pse.sheet05.presence.controlflow;
+    exports de.unistuttgart.iste.sqa.pse.sheet05.presence.controlflow;
 }
