036f75af8f41b3194edd6ea7439ce4c05d634f2c
Maukiii <mika.hepper@web.de>
2024-11-25 00:55:23 +0100

    Revert "zeile 62 ≥ auf <= geändert"
    This reverts commit 661369c62b73377b6701c8e6519b4c698f6514aa.

--- exercise-sheet-06/src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/homework/maze/AmazingPauleHamsterGame.java
+++ exercise-sheet-06/src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/homework/maze/AmazingPauleHamsterGame.java
@@ -1,13 +1,11 @@
 package de.unistuttgart.iste.sqa.pse.sheet06.homework.maze;
 
-import de.hamstersimulator.objectsfirst.datatypes.Direction;
-import de.hamstersimulator.objectsfirst.external.model.Hamster;
 import de.hamstersimulator.objectsfirst.external.simple.game.SimpleHamsterGame;
 
 /**
  * Describe the purpose of this class here.
  *
- * @author Fabian Kirschenmann, Mika Hepper
+ * @author (Your name)
  * @version (a version number or a date)
  */
 public class AmazingPauleHamsterGame extends SimpleHamsterGame {
@@ -31,39 +29,7 @@ public class AmazingPauleHamsterGame extends SimpleHamsterGame {
 		passTheMaze();
 	}
 
-	/**
-	 * Brings paule through the Maze by always sticking to the left Wall
-	 *
-	 * @requires paule has to be initialized with mouthEmpty and only at the End of the Maze there should be one Grain
-	 * @ensures paule finds his way out of the Maze and picks up the Grain
-	 */
 	void passTheMaze() {
-		Hamster paule = this.paule;
-		do {
-			paule.move();
-			paule.turnLeft(); // Checks if there is a Wall on paules left
-			if (!paule.frontIsClear()) {
-				paule.turnLeft();
-				paule.turnLeft();
-				paule.turnLeft(); //Checks if there is a Wall infront of paule
-				if (!paule.frontIsClear()) {
-					paule.turnLeft();
-					paule.turnLeft();
-					paule.turnLeft(); // Checks if there is a Wall on paules right
-					if (!paule.frontIsClear()) {
-						paule.turnLeft();
-						paule.turnLeft();
-						paule.turnLeft(); // It is a dead end and paule has to turn around
-					}
-				}
-			}
-			if (paule.grainAvailable()) {
-				paule.turnLeft();
-				paule.turnLeft(); // to face at the Exit of the Maze :)
-				paule.pickGrain();
-				paule.write("Found the Exit! :)");
-			}
-		}
-		while (paule.frontIsClear() && paule.mouthEmpty());
+		// TODO solve the maze here
 	}
 }
