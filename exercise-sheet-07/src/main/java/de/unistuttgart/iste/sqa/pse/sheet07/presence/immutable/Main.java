20982a67509705eb918c94fe13c5a77ccad98fc6
Fabian <67342990+fabian-kir@users.noreply.github.com>
2024-12-05 16:40:58 +0100

    Mistake in LazyHamsterGame.java: after the do-while the hasMoved needed to be set back to false - fixed

--- exercise-sheet-07/src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/presence/immutable/Main.java
+++ exercise-sheet-07/src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/presence/immutable/Main.java
@@ -3,6 +3,11 @@ package de.unistuttgart.iste.sqa.pse.sheet07.presence.immutable;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Implement here
+		Address neueAdresse = new Address("Hospitalstraße", 1, "70741", "Stuttgart");
+		MyImmutableStudent student = new MyImmutableStudent("Mika", 1, neueAdresse);
+
+		System.out.printf(
+				"Name lautet %s \nund die Matrikelnummer ist %d", student.getName(), student.getMatrikelnumber()
+		);
 	}
 }
