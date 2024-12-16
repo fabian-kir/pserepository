f7d354c36ec602bea808cf500657bc5885186998
Maukiii <mika.hepper@web.de>
2024-12-16 15:54:49 +0100

    added ex9

---
+++ exercise-sheet-09/src/main/java/de/unistuttgart/iste/sqa/pse/sheet09/presence/polymorphismCollections/ListTester.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.iste.sqa.pse.sheet09.presence.polymorphismCollections;
+
+/**
+ * A class to test list implementations.
+ */
+public class ListTester {
+
+	public static void main(String[] args) {
+		// Implement exercise 2 (c) and (f) here.
+	}
+
+	// Add operations for exercise 2 (b), (d) and (e) here.
+}
