4330290c8d5f0bfda1377021e54611e38a729b2d
Fabian <67342990+fabian-kir@users.noreply.github.com>
2025-01-21 09:45:00 +0100

    Grain verändert

--- exercise-sheet-11/src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/settler/Grain.java
+++ exercise-sheet-11/src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/settler/Grain.java
@@ -2,7 +2,9 @@ package de.unistuttgart.iste.sqa.pse.sheet11.homework.settler;
 
 public class Grain extends Ressource{
 
+    private static final ResourceType resourceType = new ResourceType("Grain");
+
     public Grain(String name, Position position) {
-        super(name, position, new ResourceType("Grain")); // TODO überdenken ob man da jedes mal einen neuen Resource Type erstellen sollte oder ob ma ndas mit Static löst
+        super(name, position, resourceType); // TODO überdenken ob man da jedes mal einen neuen Resource Type erstellen sollte oder ob ma ndas mit Static löst
     }
 }
