a1c97ed0a6a438b14dd5b5ca3cdfd79b6cb1c147
Fabian <67342990+fabian-kir@users.noreply.github.com>
2025-01-21 09:56:56 +0100

    Grain verändert

--- exercise-sheet-11/src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/settler/Ressource.java
+++ exercise-sheet-11/src/main/java/de/unistuttgart/iste/sqa/pse/sheet11/homework/settler/Ressource.java
@@ -3,7 +3,7 @@ package de.unistuttgart.iste.sqa.pse.sheet11.homework.settler;
 abstract class Ressource {
     public final String name;
     public final Position position;
-    public final ResourceType resourceType;
+    private final ResourceType resourceType;
 
     public Ressource (final String name, final Position position, ResourceType resourceType) {
         this.name = name;
